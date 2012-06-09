f7d03edb73f34af128b68eca54983aa2ae671b61
Ben Fulton <ben@benfulton.net>
2012-06-09 00:33:27 -0400

    Add styling, caching, index and show methods for records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,24 @@
 class RecordsController < ApplicationController
 
   def index
-    response = HTTParty.get('https://1.ravenhq.com/docs')
-    response = HTTParty.get(response.headers['oauth-source'], :headers => { "Api-Key" => key})
-    auth = "Bearer " + response.body
-    response = HTTParty.get('https://1.ravenhq.com/databases/benfulton-SourcedTriples/docs/?start=0&pageSize=10', :headers => { "Authorization" => auth })
-    @json = response.body
-    parsed_json = ActiveSupport::JSON.decode(@json)
+    loadDocs()
+    @docs = JSON.parse(Rails.cache.read('docs')).reject{|doc|doc["Source"].nil?}
+
   end
+  
+  def show
+     loadDocs()
+     @doc = JSON.parse(Rails.cache.read('docs')).find{|doc|doc["@metadata"]["@id"] == params[:id]}
+  end
+  
+  def loadDocs
+    if !Rails.cache.exist?('docs')
+      response = HTTParty.get('https://1.ravenhq.com/docs')
+      response = HTTParty.get(response.headers['oauth-source'], :headers => { "Api-Key" => APIKEY})
+      auth = "Bearer " + response.body
+      response = HTTParty.get('https://1.ravenhq.com/databases/benfulton-SourcedTriples/docs/?start=0&pageSize=10', :headers => { "Authorization" => auth })
+      Rails.cache.write('docs', response.body)
+    end
+  end
+
 end
