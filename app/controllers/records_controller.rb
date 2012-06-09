08d88f43354e585df446bd99f4db5bb5593f29ea
Ben Fulton <ben@benfulton.net>
2012-06-08 21:50:16 -0400

    API Key

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,7 +2,7 @@ class RecordsController < ApplicationController
 
   def index
     response = HTTParty.get('https://1.ravenhq.com/docs')
-    response = HTTParty.get(response.headers['oauth-source'], :headers => { "Api-Key" => "c80aa865-da7d-4c16-8444-ee09e693e9c3"})
+    response = HTTParty.get(response.headers['oauth-source'], :headers => { "Api-Key" => key})
     auth = "Bearer " + response.body
     response = HTTParty.get('https://1.ravenhq.com/databases/benfulton-SourcedTriples/docs/?start=0&pageSize=10', :headers => { "Authorization" => auth })
     @json = response.body
